70a2f48709333db4397a55651510438803a7bea3
KingdomCoder <kingdomcoding@gmail.com>
2025-05-16 10:13:29 +0100

    chore: add deps

--- config/config.exs
+++ config/config.exs
@@ -7,6 +7,9 @@
 # General application configuration
 import Config
 
+config :spark,
+  formatter: ["Ash.Resource": [section_order: [:admin]], "Ash.Domain": [section_order: [:admin]]]
+
 config :checkd,
   ecto_repos: [Checkd.Repo],
   generators: [timestamp_type: :utc_datetime]
--- lib/checkd_web/router.ex
+++ lib/checkd_web/router.ex
@@ -41,4 +41,14 @@ defmodule CheckdWeb.Router do
       forward "/mailbox", Plug.Swoosh.MailboxPreview
     end
   end
+
+  if Application.compile_env(:checkd, :dev_routes) do
+    import AshAdmin.Router
+
+    scope "/admin" do
+      pipe_through :browser
+
+      ash_admin "/"
+    end
+  end
 end
