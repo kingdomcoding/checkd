a4002b0f3763c1c78450f0e7c71b0732e862a600
KingdomCoder <kingdomcoding@gmail.com>
2025-05-17 14:17:49 +0100

    feat: update read model from event

--- lib/checkd_web/badge_user/read_models/_domain.ex
+++ lib/checkd_web/badge_user/read_models/_domain.ex
@@ -1,10 +1,23 @@
 defmodule CheckdWeb.BadgeUser.ReadModels.Domain do
   use Ash.Domain
+  use Supervisor
+
+  def start_link(arg) do
+    Supervisor.start_link(__MODULE__, arg, name: __MODULE__)
+  end
+
+  @impl true
+  def init(_arg) do
+    children = [
+      CheckdWeb.BadgeUser.ReadModels.Badges,
+    ]
+
+    Supervisor.init(children, strategy: :one_for_one)
+  end
 
   resources do
     resource CheckdWeb.BadgeUser.ReadModels.Badges
     resource CheckdWeb.BadgeUser.ReadModels.MyBadges
     resource CheckdWeb.BadgeUser.ReadModels.MyPublicBadges
   end
-
 end
--- lib/checkd_web/badge_user/read_models/badges.ex
+++ lib/checkd_web/badge_user/read_models/badges.ex
@@ -3,6 +3,10 @@ defmodule CheckdWeb.BadgeUser.ReadModels.Badges do
     data_layer: Ash.DataLayer.Ets,
     domain: CheckdWeb.BadgeUser.ReadModels.Domain
 
+  use Commanded.Event.Handler,
+    application: Checkd.Application,
+    name: __MODULE__
+
   attributes do
     attribute :id, :uuid, primary_key?: true, allow_nil?: false
     attribute :name, :string, allow_nil?: false
@@ -11,5 +15,27 @@ defmodule CheckdWeb.BadgeUser.ReadModels.Badges do
     attribute :overview, :string, allow_nil?: false
     attribute :information, Checkd.BadgeManagement.ValueObjects.Information
     attribute :offer, Checkd.BadgeManagement.ValueObjects.Offer
+  end
+
+  actions do
+    defaults [:create, :read]
+    default_accept [:id, :name, :issuer, :image, :overview, :information, :offer]
+  end
+
+  code_interface do
+    domain CheckdWeb.BadgeUser
+    define :create
+  end
+
+  def handle(%Checkd.BadgeManagement.DomainEvents.PublicBadgeCreated{} = event, _metadata) do
+    create(%{
+      id: event.id,
+      name: event.name,
+      issuer: event.issuer,
+      image: event.image,
+      overview: event.overview,
+      information: event.information,
+      offer: event.offer
+    })
   end
 end
