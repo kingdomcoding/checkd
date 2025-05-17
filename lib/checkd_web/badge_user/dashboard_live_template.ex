6767ffa375bd0d8530d442c4ce66f2428678bcf7
KingdomCoder <kingdomcoding@gmail.com>
2025-05-17 08:57:26 +0100

    feat: add qr scanner modal

--- lib/checkd_web/badge_user/dashboard_live_template.ex
+++ lib/checkd_web/badge_user/dashboard_live_template.ex
@@ -225,10 +225,37 @@ defmodule CheckdWeb.BadgeUser.DashboardLiveTemplate do
   def speed_dial(assigns) do
     ~H"""
     <div class="fixed end-6 bottom-6 group">
-        <button type="button" aria-controls="speed-dial-menu-default" aria-expanded="false" class="p-4 flex items-center justify-center text-white bg-primary-700 rounded-full w-14 h-14 hover:bg-primary-800 dark:bg-primary-600 dark:hover:bg-primary-700 focus:ring-4 focus:ring-primary-300 focus:outline-none dark:focus:ring-primary-800">
+        <button type="button" data-modal-target="qrScannerModal" data-modal-toggle="qrScannerModal" aria-controls="speed-dial-menu-default" aria-expanded="false" class="p-4 flex items-center justify-center text-white bg-primary-700 rounded-full w-14 h-14 hover:bg-primary-800 dark:bg-primary-600 dark:hover:bg-primary-700 focus:ring-4 focus:ring-primary-300 focus:outline-none dark:focus:ring-primary-800">
             <img src={~p"/images/scanner.svg"} class="h-6-" alt="Scanner" />
             <span class="sr-only">Open scanner</span>
         </button>
+    </div>
+
+    <div id="qrScannerModal" tabindex="-1" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+        <div class="relative w-full max-w-md max-h-full">
+            <!-- Modal content -->
+            <div class="relative bg-white rounded-lg shadow-sm dark:bg-gray-700">
+                <!-- Modal header -->
+                <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600 border-gray-200">
+                    <h3 class="text-xl font-medium text-gray-900 dark:text-white">
+                        QR Scanner
+                    </h3>
+                    <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="qrScannerModal">
+                        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                        </svg>
+                        <span class="sr-only">Close modal</span>
+                    </button>
+                </div>
+                <!-- Modal body -->
+                <div class="p-4 md:p-5 space-y-4">
+                    <img src={~p"/images/scanner.svg"} class="" alt="Scanner" />
+                    <p class="text-base text-center leading-relaxed text-gray-500 dark:text-gray-400">
+                        A QR scanner goes here
+                    </p>
+                </div>
+            </div>
+        </div>
     </div>
     """
   end
